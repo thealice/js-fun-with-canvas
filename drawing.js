a845fe6fb516d0b097f94fbd32ee26cfde5d58b8
Alice Freda <alicefreda@gmail.com>
2020-05-18 20:01:41 -0700

    setup canvas and add context

---
+++ drawing.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADASS';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+
